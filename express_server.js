d5e9bc2580b15461e8f6eb5be29f76cca2300d94
MichaelMini <mcho911@gmail.com>
2017-01-21 07:42:53 +0000

    Added bcrypt password feature

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 app.set("view engine", "ejs");
-var PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080; // default port 8080
 
-var cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+// const password = "purple-monkey-dinosaur"; // you will probably this from req.params
+let hashed_password = '';
+const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 app.use(function(req, res, next){
@@ -18,7 +21,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": {
   	longUrl: "http://www.lighthouselabs.ca",
   	createdBy: 'uvwxyz'
@@ -35,36 +38,26 @@ let users = {
 		password: "asdf"
 	}
 };
-
 app.use('/urls*?', (req,res, next) => {
 	if (req.user) {
 		next();
 	} else {
-		res.status(404).send('you must <a href="/login">sign in</a>');
+		res.status(404).send('You must <a href="/login">sign in</a>');
 	}
 });
 
-// app.use((req, res, next) => {
-// 	req.user = users[req.session.user_id];
-// 	next();
-// });
-
 function generateRandomString( ranLength ) {
-    var s = "";
-    while( s.length < ranLength && ranLength > 0 ){
-        var r = Math.random();
-        s+= ( r < 0.1 ? Math.floor(r*100) : String.fromCharCode( Math.floor(r*26) + (r>0.5?97:65) ) );
-    }
-    return s;
+  let s = "";
+  while( s.length < ranLength && ranLength > 0 ){
+      let r = Math.random();
+      s+= ( r < 0.1 ? Math.floor(r*100) : String.fromCharCode( Math.floor(r*26) + (r>0.5?97:65) ) );
+  }
+  return s;
 }
 
 app.get("/u/:shortURL", (req, res) => {
 	let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL].longUrl;
-  console.log('longURL: ', longURL);
-  console.log('urlDatabase: ', urlDatabase);
-  // search urlDatabase and find longURL with shortURL as the key
-  // let longURL = ...
   res.redirect(longURL);
 });
 
@@ -74,7 +67,6 @@ app.get("/", (req, res) => {
 	} else {
 		res.redirect('/login');
 	}
-  // res.end("Hello! " + req.cookies.user_id);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -90,12 +82,7 @@ app.get("/urls", (req, res) => {
 	for ( let shortURL in urlDatabase ) {
 		if (urlDatabase[shortURL].createdBy === req.user.id) {
 			userUrls[shortURL] = urlDatabase[shortURL];
-			// console.log('hello');
 		}
-		// console.log('urlDatabase: ', urlDatabase);
-		// console.log('shortURL: ', shortURL);
-		// console.log('urlDatabase[shortURL].createdBy: ', urlDatabase[shortURL].createdBy);
-		// console.log('req.user.id: ', req.user.id);
 	}
   let templateVars = { urls: userUrls, foo: 123123 };
   res.render("urls_index", templateVars);
@@ -106,27 +93,17 @@ app.get("/urls", (req, res) => {
 app.post("/urls", (req, res) => {
 	if (req.cookies.user_id) {
 		const longURL = req.body.longURL;
-		// const shortURL = generateRandomString(6);
 		const shortURL = generateRandomString(6);
 		urlDatabase[shortURL] = { 'longUrl': longURL, 'createdBy': req.cookies.user_id };
-		// console.log(urlDatabase);
-
 		res.redirect('/urls');
 	} else {
 		res.redirect('login')
 	}
-  // console.log(req.body);  // debug statement to see POST parameters
-  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
 	const shortURL = req.params.shortURL;
-  console.log('urlDatabase: ', urlDatabase);
-  console.log('shortURL: ', shortURL);
-  console.log('shortKey: ', urlDatabase[shortURL]);
   let templateVars = { username: req.cookies["username"], shortURL: shortURL, longURL: urlDatabase[shortURL].longUrl };
-  // console.log('longUrl: ', longUrl);
-  // console.log('longURL: ' longURL);
   res.render("urls_show", templateVars);
 });
 
@@ -136,7 +113,6 @@ app.post("/urls/:shortURL", (req, res) => {
 	urlDatabase[shortURL].longUrl = longURL;
 	res.redirect('/urls');
 });
-console.log(urlDatabase);
 
 // Delete
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -157,21 +133,20 @@ app.get('/register', (req, res) => {
 
 // Registration Handler
 app.post('/register', (req, res) => {
+	// you will probably this from req.params // Need to know req.params
+	hashed_password = bcrypt.hashSync(req.body.password, 10);
 	const checkEmail = Object.values(users).some((u) => u.email === req.body.email);
 	if (checkEmail || !req.body.email || !req.body.password) {
-		res.send('OMG :(', 404);
+		res.send('OMG :(... Please <a href="/register">Register </a> a different email and a proper password\n Or you can <a href="/login">Sign-in<a> to your existing account.', 404);
 	}
 	const id = generateRandomString( 6 );
 	res.cookie("user_id", id);
 	users[id] = {
 		'id': id,
 		'email': req.body.email,
-		'password': req.body.password
+		'password': hashed_password
 	};
-	const userArr = Object.values(users).find( (u) => u.email === req.body.email );
-
-	console.log('users:', users);
-	console.log('userArr:', userArr);
+	// const userArr = Object.values(users).find( (u) => u.email === req.body.email );
 	res.redirect('/');
 });
 
@@ -182,9 +157,9 @@ app.get('/login', (req, res) => {
 
 // Set Cookie Username & Login Handler
 app.post('/login', (req, res) => {
-	const matchUser = Object.values(users).find( (u) => u.email === req.body.email );
-	if (!matchUser || matchUser.password !== req.body.password) {
-		res.send('OMG :(', 403);
+	const matchUser = Object.values(users).find( (u) => u.email === req.body.email );// I need to know this
+	if (!matchUser || !bcrypt.compareSync(req.body.password, matchUser.password)) {
+		res.send('Please <a href="/login">Sign-in</a> with your correct email and password.\n Or you can <a href="/register">Register here</a> for a new account.', 403);
 	} else {
 		res.cookie("user_id", matchUser.id);
 		res.redirect('/');
@@ -194,7 +169,6 @@ app.post('/login', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
 
 
 
